298b1a4fe647ae7aa892aef770dca2ae9fd5ff2f
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-14 21:28:19 -0700

    refactor for clarity

---
+++ src/dummy-data-structures/initialQuestions.js
@@ -0,0 +1,7 @@
+export const initialQuestions = [
+  `Can you direct me to Jordan's portfolio?`,
+  "How can I learn Rivescript?",
+  "What color should we paint the DNP office?",
+  "Show me a picture.",
+  "Help me find a podcast."
+];
